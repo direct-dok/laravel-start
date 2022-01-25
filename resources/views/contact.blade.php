4780b1058a61fc4ab39476b769e4275aa65d3844
Viktor <you@example.com>
2022-01-25 21:50:46 +0500

    create db

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+</head>
+<body class="antialiased">
+
+<form action="{{ route('contact') }}" method="post">
+{{--    {{ csrf_field() }}--}}
+    @csrf
+    <input type="text" name="name">
+    <input type="email" name="email">
+    <button type="submit">Submit</button>
+</form>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,56 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+//Route::get('/', function() {
+//    return "<h1>Hello World</h1>";
+//});
+
+Route::get('/', function() {
+    $res = 2 + 3;
+    $name = 'John';
+    return view('home', compact('res', 'name'));
+});
+
+Route::get('/about', function() {
+    return '<h1>About page</h1>';
+});
+
+//Route::get('/contact', function() {
+//    return view('contact');
+//});
+//
+//Route::post('/send-email', function() {
+//    if(!empty($_POST)) {
+//        dump($_POST);
+//    }
+//    return 'Send Email';
+//});
+
+//Route::match(['post', 'get'], '/contact', function() {
+//    if(!empty($_POST)) {
+//        dump($_POST);
+//    }
+//   return view('contact');
+//});
+
+Route::match(['post', 'get'], '/contact', function() {
+    if(!empty($_POST)) {
+        dump($_POST);
+    }
+   return view('contact');
+})->name('contact');
+
+Route::view('/test', 'test', ['test' => 'data1111']);
+
+//Route::redirect('/about', '/contact');
+
+
+Route::permanentRedirect('/about', '/contact');
+
+Route::get('/post/{id}', function($id) {
+    return "Post $id";
 });
